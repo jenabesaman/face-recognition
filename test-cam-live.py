9811f8a09ca10389e42d3b15bf57004d0ced3147
saman tabrizi <94444692+jenabesaman@users.noreply.github.com>
2024-10-28 20:45:50 +0330

    update gitignore

--- test-cam-live.py
+++ test-cam-live.py
@@ -1,19 +1,3 @@
-# import cv2
-#
-# # Try a few common camera indexes
-# for i in range(10):  # Change the range based on your expectation
-#   cap = cv2.VideoCapture(i)
-#   if cap.isOpened():
-#       print(f"Camera found at index {i}")
-#       # You can break here if you only want the first camera
-#       # cap.release()  # Release the capture if not using it further
-#       break
-#   else:
-#       print(f"No camera found at index {i}")
-
-# cap.release()  # Release the capture if not closed before
-
-
 import cv2
 
 # Replace 0 with the index of your camera if you have multiple cameras
